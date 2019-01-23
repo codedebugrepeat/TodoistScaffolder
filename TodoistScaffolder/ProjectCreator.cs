5e72dd33187233f6cd9cdc60aa475fe75ab25e15
Christoph Krenn <c.j.krenn@gmail.com>
2019-01-23 18:42:48 +0100

    Add new files pushing up to 300 projects in 3 batches; add creator to scaffold json for 300 projects

---
+++ TodoistScaffolder/ProjectCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoistScaffolder
+{
+    public class ProjectCreator
+    {
+        public string CreateJsonFor300Projects()
+        {
+            const int noProjects = 300;
+            var stringBuilder = new StringBuilder("[");
+            for (var i = 1; i <= noProjects; i++)
+            {
+                var project = $"{{\"type\": \"project_add\",\"temp_id\": \"Pre78961a3910d4f1489caf38e11e9c6de\",\"args\": {{\"name\": \"{i}\",\"item_order\": {i},\"indent\": {new Random().Next(1, 3)},\"color\": 42}},\"uuid\": \"\"}}";
+                stringBuilder.Append(project);
+                if (i < noProjects)
+                    stringBuilder.Append(",");
+            }
+            return stringBuilder.ToString();
+        } 
+    }
+}
